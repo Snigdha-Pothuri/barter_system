1ae5d0707cbb3cf3dca806f3ee5d3761d362147a
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2020-12-27 16:57:53 +0530

    Add files via upload

---
+++ CustomSideBarMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View,Text,TextInput,StyleSheet,TouchableOpacity} from 'react-native';
+import {DrawerItems} from 'react-navigation-drawer';
+import firebase from 'firebase';
+
+export default class CustomSideBarMenu extends React.Component {
+    render () {
+        return (
+            <View style={{flex : 1}}>
+            <DrawerItems {...this.props}/>
+            <View style={{flex:1,justifyContent:"flex-end",paddingBottom:30}}>
+                 <TouchableOpacity style={{justifyContent:"center",padding:10,height:30,width:"100%"}}
+                 onPress={()=>{
+                     this.props.navigation.navigate("SignUpLoginScreen")
+                     firebase.auth().signOut()
+                 }}
+                 >
+                     <Text> Log Out </Text>
+                 </TouchableOpacity>
+            </View>
+            </View>
+        )
+    }
+}
