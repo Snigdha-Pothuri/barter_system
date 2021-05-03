6b213ba88faee4619f7d7ffba2ad5112f880efcf
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-05-03 16:17:40 +0530

    Update AppTabNavigator.js

--- components/AppTabNavigator.js
+++ components/AppTabNavigator.js
@@ -2,8 +2,7 @@ import HomeScreen from '../screens/HomeScreen';
 import ExchangeScreen from '../screens/ExchangeScreen';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import { createAppContainer, createSwitchNavigator} from 'react-navigation';
-const AppContainer = createAppContainer(switchNavigator);
-const AppTabNavigator = createBottomTabNavigator({
+export const AppTabNavigator = createBottomTabNavigator({
  HomeScreen : {
       screen :HomeScreen,
   },
