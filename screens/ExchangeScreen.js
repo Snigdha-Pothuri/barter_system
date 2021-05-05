00f7529790e4c9c400ff4caa692da539166f2671
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-05-05 16:49:52 +0530

    Update ExchangeScreen.js

--- screens/ExchangeScreen.js
+++ screens/ExchangeScreen.js
@@ -1,97 +1,227 @@
 import React from 'react';
-import { View,Text,TextInput,StyleSheet,TouchableOpacity,Alert,} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, KeyboardAvoidingView, Alert, TouchableOpacity} from 'react-native';
+import MyHeader from '../components/MyHeader';
 import db from '../config';
 import firebase from 'firebase';
-import MyHeader from '../components/MyHeader';
 
-export default class ExchangeScreen extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            userName : firebase.auth().currentUser.email,
-            description : "",
-            itemName : ""
-                }
-    }
-    createUniqueId () {
-        return Math.random().toString(36).substring(7); 
+export default class BookRequestScreen extends React.Component {
+   constructor(){
+     super();
+     this.state = {
+       userId : firebase.auth().currentUser.email,
+       itemName : "",
+       reasonToRequest : "",
+       isExchangeRequestActive : "",
+       requestedBookName : "",
+       bookStatus : "",
+       requestId : "",
+        userDocId : "",
+        docId : "",
+
+     }
+   }
+   createUniqueId(){
+     return Math.random().toString(36).substring(7);
+   }
+   addRequest = async (itemName,reasonToRequest)=>
+   {
+     var userId = this.state.userId
+     var randomRequestId = this.createUniqueId();
+     db.collection("requested_books").add({
+       "user_id" : userId,
+       "book_Name": itemName,
+       "reasonToRequest": reasonToRequest,
+       "requestId" : randomRequestId,
+       "book_status":"requested",
+       "date" : firebase.firestore.FieldValue.serverTimestamp()
+     })
+     await this.getBookRequest()
+     db.collection("users").where("email_id","==",userId).get()
+     .then((snapshot)=>{
+          snapshot.forEach((doc)=>{
+            db.collection("users").doc(doc.id).update({isExchangeRequestActive : true})
+          })
+     })
+     this.setState ({
+      itemName : "",
+       reasonToRequest : "",
+      requestId : randomRequestId
+     })
+    return Alert.alert("Book Succesfully Requested")
     } 
-    addItem = (itemName,description) => {
-        var userName = this.state.userName
-        db.collection("exchanged_requests").add({
-       "userName" : userName,
-       "itemName" : itemName,
-       "description" : description,
-       exchangeId : this.createUniqueId()
+    receivedBooks = (book_Name) => {
+      var userId  = this.state.userId
+      var requestId = this.state.requestId
+      db.collection("received_books").add({
+        "user_id" : userId,
+        "book_Name" : book_Name,
+        "request_id" : requestId,
+       "bookStatus" : "received"
+      })
+    } 
+    getIsBookRequestActive () {
+      db.collection("users").where("email_id","==",this.state.userId)
+      .onSnapshot((snapshot)=>{
+        snapshot.forEach((doc)=>{
+          this.setState({
+            isExchangeRequestActive : doc.data().isExchangeRequestActive,
+            userDocId : doc.id
+          })
         })
-        return alert("Item ready to exchange"," ",[{text : 'OK' , onPress:()=>{ this.props.navigation.navigate("HomeScreen")}} ])
-    }
-    render () {
-        return (
-            <View> 
-                 <MyHeader  
-        title = "Exchange an item"
+      })
+    } 
+    getBookRequest = () => {
+      var bookRrequest = db.collection("requested_books")
+      .where("user_id","==",this.state.userId)
+      .get()
+      .then((snapshot)=> {
+        snapshot.forEach((doc)=> {
+          if(doc.data().book_status !== "received") {
+            this.setState({
+              requestId : doc.data().requested_id,
+              requestedBookName : doc.data().book_Name,
+              bookStatus : doc.data().book_status,
+              docId : doc.id
+            })
+          }
+        })
+      })
+    } 
+    sendNotification = () => {
+      db.collection("users").where("email_id","==",this.state.userId).get()
+      .then((snapshot)=> {
+        snapshot.forEach((doc)=> {
+          var name = doc.data().first_name
+          var last_name = doc.data().last_name
+          db.collection("all_notifications").where("exchangeId","==",this.state.requestId).get()
+          .then((snapshot)=>{
+            snapshot.forEach((doc)=>{
+              var donorId = doc.data().donorId
+              var itemName = doc.data().itemName
+              db.collection("all_notifications").add({
+                "targeted_user_id" : donorId,
+                "message" : name + " " + last_name + "Received the item" + itemName,
+                "notification_status" : "unread",
+                "itemName" : itemName
+              })
+            })
+          })
+        })
+      })
+    } 
+    componentDidMount () {
+      this.getBookRequest();
+      this.getIsBookRequestActive();
+    } 
+  updateBookRequestStatus = () => {
+    db.collection("requested_books").doc(this.state.docId)
+    .update({
+    book_status : "Received"
+  }) 
+  db.collection("users").where("email_id","==",this.state.userId).get()
+  .then((snapshot)=>{
+    snapshot.forEach((doc)=>{
+      db.collection("users").doc(doc.id).update({
+        isExchangeRequestActive : false
+      })
+    })
+  })
+  } 
+  
+  render(){
+    if(this.state.isExchangeRequestActive === true){
+      return (
+        <View style={{flex:1,justifyContent:"center"}}>
+           <Text>
+             Book Name
+           </Text>
+           <Text> {this.state.requestedBookName} </Text>
+           <Text> Book Status </Text>
+           <Text> {this.state.bookStatus} </Text>
+           <TouchableOpacity style={{borderWidth:1,backgroundColor:"yellow",alignSelf:"center",width:300}}
+           onPress={()=>{this.sendNotification()
+          this.updateBookRequestStatus()
+          this.receivedBooks(this.state.requestedBookName)}}
+           >
+<Text> I received the book </Text> 
+           </TouchableOpacity>
+        </View>
+      )
+    } 
+  else {
+
+ 
+    return (
+       
+      <View style={{flex:1}}>
+      <MyHeader  
+        title = "Request Book"
         navigation= {this.props.navigation}
       />
-                <TextInput
-style={styles.textInput}
-placeholder ={"Item Name"}
-onChangeText={(text)=>{ 
-    this.setState({ itemName: text
-     }) 
-     }}
-/>
-<TextInput
-placeholder ={"Item Description"}
-onChangeText={(text)=>{
-     this.setState({ description: text
-      })
-       }}
-/> 
+      <KeyboardAvoidingView style={StyleSheet.keyboardStyle}>
+       <TextInput 
+          style={styles.formTextInput}
+          placeholder = {"Enter Book Name"}
+          onChangeText={(text)=>{
+            this.setState ({
+              itemName : text
+            })
+          }}
+          value = {this.state.itemName}
+        
+       />
 
- <TouchableOpacity  style={styles.addItemButton}
- onPress={()=>{this.addItem(this.state.itemName,this.state.description)}}
- >
-     <Text style={styles.text}> Add Item </Text>
- </TouchableOpacity>
-            </View>
-        )
-    }
+<TextInput 
+          style={styles.formTextInput}
+          multiline
+          numberOfLines={8}
+          placeholder = {"Why do you want this book?"}
+          onChangeText={(text)=>{
+            this.setState ({
+          reasonToRequest    : text
+            })
+          }}
+          value = {this.state.reasonToRequest}
+        
+       />
+
+       <TouchableOpacity style={styles.button} onPress={()=>{this.addRequest(this.state.itemName,this.state.reasonToRequest)}}>
+       <Text>Request</Text>
+       </TouchableOpacity>
+      </KeyboardAvoidingView>
+     
+      </View>
+    
+    );
+  }
+  }
 } 
-
 const styles = StyleSheet.create({
-    textInput:{
-        width:"75%",
-        height:35,
-        alignSelf:'center',
-        borderColor:'#ffab91',
-        borderRadius:10,
-        borderWidth:1,
-        marginTop:20,
-        padding:10
-      },
-      formTextInput:{
-        width:"75%",
-        height:100,
-        alignSelf:'center',
-        borderColor:'#ffab91',
-        borderRadius:10,
-        borderWidth:1,
-        marginTop:20,
-        padding:10
-      },
-      addItemButton:{
-        width:200,
-        height:40,
-        alignItems:'center',
-        justifyContent:'center',
-        borderWidth:1,
-        borderRadius:10,
-        marginTop:30
-      },
-      text : {
-          color : "#ffff",
-          fontSize : 18,
-          fontWeight: "bold" 
-      }
+ KeyboardAvoidingView:{
+   flex:1,
+   justifyContent:'center',
+   alignItems:'center'
+ },
+ button:{
+   width:"75%",
+   height:35,
+   justifyContent:'center',
+   alignItems:'center',
+   borderRadius:5,
+   backgroundColor:"#ff9800",
+   shadowColor: "#000",
+   shadowOffset: {
+      width: 0,
+      height: 8,
+   }
+  },
+  formTextInput : {
+    width:"75%",
+    height : 35,
+    alignSelf : "center",
+    borderRadius:10,
+    borderWidth:1,
+    marginTop : 20,
+    padding:10
+  }
 })
